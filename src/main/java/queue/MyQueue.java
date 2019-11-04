bcaff0c26e4e6bbe95cabcf6e742c94ac7943407
Deepa Shivakumar <dshivaku@jcp.com>
2019-11-04 23:39:26 +0530

    Queue implemted with linked list

---
+++ src/main/java/queue/MyQueue.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2018 JCPenney Co. All rights reserved.
+ */
+package queue;
+
+/**
+ * Class for MyQueue.
+ */
+public class MyQueue<T> {
+
+  private Node<T> head;
+  private Node<T> tail;
+
+  public void enqueue(T data) {
+    Node<T> node = new Node<>(data,null);
+    if ( tail == null){
+        head = tail = node;
+    }
+    else {
+      tail.next= node;
+      tail = node;
+    }
+  }
+
+  public T dequeue() {
+    T data = null;
+    if (head.next == null){
+      data = head.data;
+      head = tail = null;
+      return data;
+    }
+    else {
+      data = head.data;
+      Node<T> node =  head.next;
+      head = node;
+    }
+    return data;
+
+  }
+
+  public boolean isEmpty() {
+    return head == null && tail == null;
+  }
+
+  private static class Node<T> {
+
+    private T data;
+    private Node<T> next;
+
+    public Node(T data, Node<T> next) {
+      this.data = data;
+      this.next = next;
+    }
+  }
+
+}
---
+++ src/main/java/queue/Runner.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018 JCPenney Co. All rights reserved.
+ */
+package queue;
+
+import stack.MyStack;
+
+/**
+ * Class for Runner.
+ */
+public class Runner {
+
+  public static void main(String[] args) {
+    MyQueue queue = new MyQueue();
+    queue.enqueue("hello1");
+    queue.enqueue("hello2");
+    queue.enqueue("hello3");
+    queue.enqueue("hello4");
+    System.out.println(queue.dequeue());
+    System.out.println(queue.dequeue());
+    System.out.println(queue.dequeue());
+    System.out.println(queue.isEmpty());
+    System.out.println(queue.dequeue());
+    System.out.println(queue.isEmpty());
+  }
+
+}
